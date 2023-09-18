cc1968fdb2f6b83d2059f057e9e1f8d6e5554b79
UvarovIV <post689@mail.ru>
2023-09-18 23:44:38 +0300

    update RoleRepository and UserRepository

--- backend/src/main/java/ru/sber/delivery/repositories/RoleRepository.java
+++ backend/src/main/java/ru/sber/delivery/repositories/RoleRepository.java
@@ -3,10 +3,11 @@ package ru.sber.delivery.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.sber.delivery.entities.Role;
+import ru.sber.delivery.entities.enum_model.ERole;
 
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(ERole name);
 }
--- backend/src/main/java/ru/sber/delivery/repositories/UserRepository.java
+++ backend/src/main/java/ru/sber/delivery/repositories/UserRepository.java
@@ -11,6 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByEmailAndPassword(String email, String password);
 
+    Optional<User> findByUsername(String username);
+
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
