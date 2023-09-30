bc35c42c3a7eea1c5ce21cdc909a58d9b5a2b5c1
kes20 <ke.s2013@yandex.ru>
2023-09-30 23:32:58 +0300

    add yandex map

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { YMaps, Map } from 'react-yandex-maps';
+
+function App() {
+    return (
+        <YMaps query={{ apikey: '8ec18778-cb70-437f-87fc-7c17e8e0bb71'}}>
+            <div className="App">
+                <h1>Мои карты</h1>
+                <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+            </div>
+        </YMaps>
+    );
+}
+
+export default App;
