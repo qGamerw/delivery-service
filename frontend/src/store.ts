31101fce95c9bd7106df6a353823686915c14fc2
Petrov-V-V <vp6514764@gmail.com>
2023-10-04 19:52:09 +0300

    add shift slice & service

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import shiftReducer from './slices/shiftSlice';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
-
+        shift: shiftReducer,
     },
 });
 
